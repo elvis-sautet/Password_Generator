7ddca33db3dd9b1010782fa751713d644f3f80e6
elvis-sautet <60774154+elvis-sautet@users.noreply.github.com>
2020-09-12 23:28:19 +0300

    updated using boostrap

---
+++ index.js
@@ -0,0 +1,76 @@
+//get the Dom elements;
+const outputPass = document.getElementById("output");
+const clipboard = document.getElementById("fa-clipboard");
+const passLength = document.getElementById("length");
+const upperCase = document.getElementById("upper");
+const lowerCase = document.getElementById("lower");
+const numberStr = document.getElementById("number");
+const symbolStr = document.getElementById("symbol");
+const buttonGeneratePaswword = document.getElementById("generate");
+const clipboardtxt = document.getElementById("copy");
+//assign the random generator functions to an object
+const randomFunc = {
+  hasUpper: getUpper,
+  hasLower: getLower,
+  hasNumber: getNumber,
+  hasSymbol: getSymbol,
+};
+
+buttonGeneratePaswword.addEventListener("click", () => {
+  const length = +passLength.value;
+  const hasUpper = upperCase.checked;
+  const hasLower = lowerCase.checked;
+  const hasNumber = numberStr.checked;
+  const hasSymbol = symbolStr.checked;
+
+  let generatedPassword = "";
+  const totalChecked = hasUpper + hasLower + hasNumber + hasSymbol;
+  const typeArray = [
+    { hasUpper },
+    { hasLower },
+    { hasNumber },
+    { hasSymbol },
+  ].filter((type) => Object.values(type)[0]);
+  if (totalChecked === 0) {
+    return "";
+  }
+  for (let i = 0; i < length; i += totalChecked) {
+    typeArray.forEach((item) => {
+      const functName = Object.keys(item)[0];
+      generatedPassword += randomFunc[functName]();
+    });
+  }
+  // slice the password to the amount of length specified
+  const slicedpass = generatedPassword.slice(0, length);
+  outputPass.innerHTML = slicedpass;
+});
+//event listener to copy password to clipboard
+clipboardtxt.addEventListener("click", () => {
+  const password = outputPass.innerText;
+  if (!password) {
+      swal ("opps!","Cannot copy blank password","error");
+    return;
+  }
+  const textArea = document.createElement("textarea");
+  textArea.value = password;
+  document.body.appendChild(textArea);
+  textArea.select();
+  document.execCommand("copy");
+  textArea.remove();
+  swal("success","Password copied to clipboard","success");
+});
+
+//functions to return random characters from a charctercode
+function getUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+function getLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+function getNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+function getSymbol() {
+  const symbol = "!@#$%^&*()_+";
+  return symbol[Math.floor(Math.random() * symbol.length)];
+}
--- index.php
+++ index.php
@@ -5,45 +5,54 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href='https://fonts.googleapis.com/css?family=Lora' rel='stylesheet'>
-    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="./bootstrap.min.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.1/css/all.css" integrity="sha384-xxzQGERXS00kBmZW/6qxqJPyxW3UR0BPsL4c8ILaIWXva5kFi7TxkIIaMiKtqV1Q" crossorigin="anonymous">
+    <script src="./jquerycodeforall.js"></script>
+    <script src="sweetalert.min.js"></script>
     <title>Password_Generator</title>
 </head>
 
-<body>
-    <div class="overrall-container">
-            <h2>Password Generator</h2>
-        <div class="inside_container">
-        <i class="fas fa-clipboard"  id="copy" style="float: right;margin-right:3px;font-size:20px"></i>
-            <div class="outputselect">
-                <span id="output"></span>
-            </div>
-            <div class="inputscheckbox">
-                <label for="upercase">Password Length</label>
-                <input type="number" id="length" value="20" max="26">
-                <div class="inputscheckbox">
-                    <label for="upercase"> UpperCase characters</label>
-                    <input type="checkbox" id="upper" checked>
-                </div>
-                <div class="inputscheckbox">
-                    <label for="upercase"> LowerCase characters</label>
-                    <input type="checkbox" id="lower" checked>
-                </div>
-                <div class="inputscheckbox">
-                    <label for="upercase"> Number characters</label>
-                    <input type="checkbox" id="number" checked>
-                </div>
-                <div class="inputscheckbox">
-                    <label for="upercase"> symbol characters</label>
-                    <input type="checkbox" id="symbol" checked>
-                </div>
-                <div class="btngenetator">
-                <button id="generate">Generate Password</button>
-                </div>
-            </div>
+<body class="bg-secondary">
+    <div class="container list-group-item mt-5 rounded" style="width: fit-content;overflow:hidden;">
+        <h2 class="bg-secondary text-center text-white img-thumbnail">Password Generator</h2>
+        <p class="text-center"><small class="text-capitalize text-primary">by sautet ...</small></p>
+        <div class="form-group">
+            <i class="fas fa-clipboard float-right btn-lg mr-0" id="copy" style="cursor: pointer;z-index:1000;"></i>
+            <span class="form-control text-secondary border-primary" id="output" style="white-space:nowrap;"></span>
         </div>
+        <div class="form-group m-0 font-weight-bolder">
+            <label class="text-capitalize text-primary">Password Length</label>
+            <input type="number" id="length" class="float-right form-control p-0 border-primary" style="width: 20%;height:fit-content" value="20" max=20>
+            <script>
+                var fieldLength = document.getElementById("length");
+                fieldLength.addEventListener("change",()=>{
+                    if(fieldLength.value > 20){
+                        fieldLength.value = 20;
+                    }
+                });
+            </script>
+        </div>
+        <div class="clearfix"></div>
+        <div class="form-group m-0 font-weight-bolder">
+            <label class="text-capitalize text-primary"> UpperCase characters</label>
+            <input type="checkbox" id="upper" class="float-right" checked>
+        </div>
+        <div class="form-group m-0 font-weight-bolder">
+            <label class="text-capitalize text-primary"> LowerCase characters</label>
+            <input type="checkbox" id="lower" class="float-right" checked>
+        </div>
+        <div class="form-group m-0 font-weight-bolder">
+            <label class="text-capitalize text-primary"> Number characters</label>
+            <input type="checkbox" id="number" class="float-right" checked>
+        </div>
+        <div class="form-group m-0 font-weight-bolder">
+            <label class="text-capitalize text-primary"> symbol characters</label>
+            <input type="checkbox" id="symbol" class="float-right" checked>
+        </div>
+        <button type="submit" class="btn btn-primary font-weight-bold" id="generate">Generate Password</button>
+        
     </div>
-<script src="app.js"></script>
+    <script src="index.js"></script>
 </body>
 
 </html>
